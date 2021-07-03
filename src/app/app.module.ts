2d34cd8273df401e68e7ddbfde66d1e0efb23220
anuragsingh228 <anurags228@gmail.com>
2021-07-03 22:29:25 +0530

    basic setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { BlogListComponent } from './blog/blog-list/blog-list.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { QuillModule } from 'ngx-quill';
+import { CreateBlogComponent } from './blog/create-blog/create-blog.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { SingleBlogComponent } from './blog/single-blog/single-blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    BlogListComponent,
+    NavbarComponent,
+    CreateBlogComponent,
+    SingleBlogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    QuillModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
